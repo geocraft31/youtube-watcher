d1fc5d84b14733baace5bf93fb3488322fde8070
geocraft31 <geocraft31@gmail.com>
2024-08-10 00:11:35 +0200

    Fix: get audio data from another package

--- audio.js
+++ audio.js
@@ -1,82 +1,61 @@
+const ytdl = require("youtube-dl-exec");
+const ffmpeg = require("fluent-ffmpeg");
+const Speaker = require("speaker");
 
-const play = require("play-dl")
-const { opus } = require("prism-media")
-const Speaker = require("speaker")
-const { hyperlink, sleep } = require("./util.js")
-
+const {
+  hyperlink,
+  sleep,
+  searchYoutube,
+  getVideoDurationInSeconds,
+} = require("./util.js");
 
 async function getVideoData(query) {
-  const queryType = await play.validate(query);
-
-  if (!queryType)
-    throw Error(
-      `Given querry is not a youtube link nor a serach item: ${query}`,
-    );
-
-  if (!queryType.startsWith("yt") && queryType != "search") {
-    throw Error(
-      `Given querry is not a youtube link nor a serach item: ${query}`,
-    );
-  }
-
-
-  if (queryType == "yt_video" || queryType == "search") {
-    const searchResult = await play.search(query, {
-      source: { youtube: "video" },
-      limit: 1
-    });
-    return searchResult;
-  }
-  else if (queryType == "yt_playlist") {
-    const playlist = await play.playlist_info(query)
-    const videos = await playlist.all_videos()
-    return videos
-  }
-
+  const searchResult = await searchYoutube(query);
+  return searchResult;
 }
 
 async function playAudioFromVideo(query) {
   const videoList = await getVideoData(query);
-  const video = videoList[0]
+  const video = videoList[0];
 
-const opusDecoder = new opus.Decoder({
-    channels: 2,
-    rate: 48000,
-    frameSize: 960,
-});
+  const channelId =
+    video.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.browseId;
 
-const speaker = new Speaker({
+  const speaker = new Speaker({
     channels: 2,
     bitDepth: 16,
     sampleRate: 48000,
-});
-    try {
-        var source = await play.stream(video.url) ;
-    } catch {
-        console.error(`Error while getting video data`)
-    }
-
-  console.log(`Now playing: ${hyperlink(video.url, video.title)}`)
-  console.log(`From:        ${hyperlink(video.channel.url, video.channel.name)}`)
-
-  source.stream.on("data", (data) => {
-    opusDecoder.write(data);
   });
+  try {
+    var source = ytdl.exec(`https://www.youtube.com/watch?v=${video.id}`, {
+      output: "-",
+      audioFormat: "opus",
+      extractAudio: true,
+    });
+  } catch {
+    console.error(`Error while getting video data`);
+  }
 
-  source.stream.on("finish", () => {
-    source.stream.end();
-    opusDecoder.end()
-    speaker.end();
-  });
+  ffmpeg(source.stdout)
+    .audioCodec("pcm_s16le")
+    .audioFilters("aresample=48000")
+    .format("s16le")
+    .pipe(speaker, { end: true });
 
-  opusDecoder.on("data", (decodedData) => {
-    speaker.write(decodedData);
-  });
-  
-  await sleep((video.durationInSec) * 1000)
+  console.log(
+    `Now playing: ${hyperlink(`https://www.youtube.com/watch?v=${video.id}`, video.title)}`,
+  );
+  console.log(
+    `From:        ${hyperlink(
+      `https://www.youtube.com/channel/${channelId}`,
+      video.channelTitle,
+    )}`,
+  );
 
+  const duration = getVideoDurationInSeconds(video.length.simpleText);
+  await sleep(duration * 1000);
 }
 
 module.exports = {
-  playAudioFromVideo: playAudioFromVideo
-}
+  playAudioFromVideo: playAudioFromVideo,
+};
--- util.js
+++ util.js
@@ -1,66 +1,89 @@
-const fs = require('fs')
-const { fileURLToPath } = require("url")
-const path = require("path")
+const fs = require("fs");
+const ytSearch = require("youtube-search-api");
+const { fileURLToPath } = require("url");
+const path = require("path");
 
 const filePath = path.resolve(__filename, "..");
 
+function getVideoDurationInSeconds(duration) {
+  var durationInSeconds = 0;
+  const times = duration.split(":");
+
+  for (i = 0; i < times.length; i++) {
+    durationInSeconds += Number(times[i]) * 60 ** (times.length - 1 - i);
+  }
+
+  return durationInSeconds;
+}
+
+async function searchYoutube(query) {
+  try {
+    const results = await ytSearch.GetListByKeyword(query, false);
+    return results.items;
+  } catch (error) {
+    console.error("Error searching YouTube:", error);
+    return [];
+  }
+}
+
 function getPlaylistData() {
-    try {
-        const data = fs.readFileSync(`${filePath}/playlist.json`, 'utf-8')
-        return JSON.parse(data)
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    const data = fs.readFileSync(`${filePath}/playlist.json`, "utf-8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function writePlaylistData(data) {
-    try {
-        if (typeof data != "string") {
-            data = JSON.stringify(data)
-        }
-
-        fs.writeFileSync(`${filePath}/playlist.json`, data, {encoding: 'utf-8'})
-    } catch (err) {
-        console.error(err)
+  try {
+    if (typeof data != "string") {
+      data = JSON.stringify(data);
     }
+
+    fs.writeFileSync(`${filePath}/playlist.json`, data, { encoding: "utf-8" });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms))
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function hyperlink(url, text) {
-    return `\x1b]8;;${url}\x1b\\${text}\x1b]8;;\x1b\\`;
+  return `\x1b]8;;${url}\x1b\\${text}\x1b]8;;\x1b\\`;
 }
 
 function shuffleList(list) {
-    try {
-        if(list.length <= 2) {
-            return console.log("Not enough songs to shuffle")
-        }
-
-        let currentIndex = list.length
-        let randomIndex = undefined
-        
-        while (currentIndex != 1) {
-            randomIndex = Math.floor(Math.random() * currentIndex)
-            currentIndex--
-            
-            let temp = list[currentIndex]
-            list[currentIndex] = list[randomIndex]
-            list[randomIndex] = temp
-        }
-
-    } catch (err) {
-        console.log("No songs to shuffle")
-        console.log(err)
+  try {
+    if (list.length <= 2) {
+      return console.log("Not enough songs to shuffle");
     }
+
+    let currentIndex = list.length;
+    let randomIndex = undefined;
+
+    while (currentIndex != 1) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      let temp = list[currentIndex];
+      list[currentIndex] = list[randomIndex];
+      list[randomIndex] = temp;
+    }
+  } catch (err) {
+    console.log("No songs to shuffle");
+    console.log(err);
+  }
 }
 
 module.exports = {
-    getPlaylistData: getPlaylistData,
-    writePlaylistData: writePlaylistData,
-    sleep: sleep,
-    hyperlink: hyperlink,
-    shuffleList: shuffleList
-}
+  getPlaylistData: getPlaylistData,
+  writePlaylistData: writePlaylistData,
+  sleep: sleep,
+  hyperlink: hyperlink,
+  shuffleList: shuffleList,
+  searchYoutube: searchYoutube,
+  getVideoDurationInSeconds: getVideoDurationInSeconds,
+};
